f5b66f8efbe18c79f1e700098c086e16afd512ad
Алекс <chu@mail.ru>
2022-09-25 15:29:47 +0300

    домашка 3,задания 1,2,3

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,7 +2,7 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
+        var dog = 8.0;
         var cat = 3.6;
         var papper = 763789;
         System.out.println(dog);
@@ -14,7 +14,14 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(papper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        papper = papper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(papper);
 
     }
+
 
 }
